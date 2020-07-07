44d3bcc648b16b197d4ec95ef3e0582a025ba2be
Rob Nichols <rob.nichols@digital.cabinet-office.gov.uk>
2020-07-07 09:59:49 +0100

    Update rake version

--- lib/workflow_to_hash/version.rb
+++ lib/workflow_to_hash/version.rb
@@ -1,3 +1,8 @@
 module WorkflowToHash
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
+
+# Change Log
+# ----------
+# 0.1.1 - Updates Rake version to avoid vulnerability (CVE-2020-8130) 
+# 0.1.0 - Initial working version
